3c13e95a740ec4faed4caf59d8e315443849802c
xiaoji121 <dong.fly.ming@gmail.com>
2013-04-18 19:32:04 +0800

    ff

---
+++ assets/custom_style/js/postlist.js
@@ -0,0 +1,22 @@
+/**
+ * 控制最近文章列表长度
+ */
+
+;(function($) {
+	var app = {
+		init: function() {
+			var post = $('aside .bullet li');
+			post.each(function( index, ele ) {
+				if ( index > 14 ) {
+					$(ele).hide();
+				}
+			});
+		},
+
+		end: 0
+	};
+
+	$(function() {
+		app.init();
+	});
+})(jQuery);
--- assets/custom_style/js/screen/style.js
+++ assets/custom_style/js/screen/style.js
@@ -9,3 +9,4 @@
 /*merge end*/
 ImportJavscript.url("http://127.0.0.1:4000/assets/custom_style/js/frame.js");
 ImportJavscript.url("http://127.0.0.1:4000/assets/custom_style/js/prettify.js");
+ImportJavscript.url("http://127.0.0.1:4000/assets/custom_style/js/postlist.js");
